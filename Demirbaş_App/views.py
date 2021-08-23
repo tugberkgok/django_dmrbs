3f93c29c777017a56ab8ce1b59f1fe96c7581709
tugberk <tugberk5858@gail.com>
2021-08-23 15:51:04 +0300

    Version 1.0.1

--- Demirbaş_App/views.py
+++ Demirbaş_App/views.py
@@ -20,7 +20,7 @@ def Main(request):
     data = Worker.objects.all()
     return render(request, "Main.html", {"veri": data})
 
-
+@login_required(login_url = 'login')
 def update(request, id):
     datas = Device.objects.filter(person_id=id)
     person = Worker.objects.filter(id=id)
@@ -30,6 +30,7 @@ def update(request, id):
     except:
         return render(request, "update.html", {"name": person[0]})
 
+@login_required(login_url = 'login')
 def delete(request, id):
     worker = get_object_or_404(Worker, id=id)
     worker.delete()
@@ -54,6 +55,7 @@ def logoutUser(request):
     logout(request)
     return redirect("/")
 
+@login_required(login_url = 'login')
 def addPerson(request):
     form = WorkerName(request.POST or None)
     if form.is_valid():
@@ -78,6 +80,7 @@ def addPerson(request):
         return render(request, "addPerson.html", {"form": form})
 
 
+@login_required(login_url = 'login')
 def addData(request, id):
     person = Worker.objects.filter(id=id)
     form = DataForm(request.POST or None)
@@ -99,6 +102,7 @@ def addData(request, id):
     return render(request, "addData.html", {"form": form})
 
 
+@login_required(login_url = 'login')
 def objectEdit(request, id):
     datas = Device.objects.filter(id=id)
     conn = sqlite3.connect('D:/C den/Masaüstü/Çalışma/Py/Demirbaş Web/Demirbaş_Web/db.sqlite3')
@@ -114,6 +118,7 @@ def objectEdit(request, id):
 
     return render(request, "objectEdit.html", {"form": form})
 
+@login_required(login_url = 'login')
 def objectDelete(request, id):
     object = Device.objects.filter(id=id)
     conn = sqlite3.connect('D:/C den/Masaüstü/Çalışma/Py/Demirbaş Web/Demirbaş_Web/db.sqlite3')
